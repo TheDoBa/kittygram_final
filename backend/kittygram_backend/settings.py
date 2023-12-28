d6def3127dcebb69a3cddac9d4225878f302388e
Vladimir Avizhen <vladasd@tut.by>
2023-12-28 16:34:31 +0300

    5 commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -95,6 +95,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -114,6 +115,3 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
