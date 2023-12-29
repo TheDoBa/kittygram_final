e9b9cdfa8509368d1a7d1417d8df093215ddefb4
Vladimir Avizhen <vladasd@tut.by>
2023-12-29 14:38:31 +0300

    2 review v3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'test_secret_key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
